2a07b9bdda6db4fdd0ef5755d9fe00f3fc143f76
Armin <kzardkoohi@gmail.com>
2019-06-29 21:41:00 -0500

    adding calculator function

---
+++ lib/classes/calculator.dart
@@ -0,0 +1,13 @@
+import 'package:bmi_calc/constants.dart';
+
+class Calculator {
+  double bmi({
+    Gender gender,
+    MeasurementSystem measurementSystem,
+    double height,
+    double weight,
+    int age,
+  }) {
+    return 0.0;
+  }
+}
